a44e6265a1f25e89997f0a96ce8a2631d8d1b260
ghostfirex22 <lokmanefire@gmail.com>
2024-03-05 16:55:48 +0100

    fix a bug

---
+++ projects/settings/settings.js
@@ -0,0 +1,5 @@
+document.querySelector(".form-check-input").addEventListener('click',()=>{
+    
+    let mode_color = document.querySelector("body")
+    mode_color.style.backgroundColor = "white"
+})
